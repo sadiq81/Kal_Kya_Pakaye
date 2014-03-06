201a59ed44db0a3367ab720488d07fb5db7824a2
Tommy Sadiq Hinrichsen <tsh@trifork.com>
2014-03-06 16:45:14 +0100

    Initial Commit

---
+++ Kal Kya Pakaye/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kal Kya Pakaye
+//
+//  Created by Tommy Sadiq Hinrichsen on 06/03/14.
+//  Copyright (c) 2014 Tommy Sadiq Hinrichsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Kal Kya PakayeTests/Kal_Kya_PakayeTests.m
@@ -0,0 +1,34 @@
+//
+//  Kal_Kya_PakayeTests.m
+//  Kal Kya PakayeTests
+//
+//  Created by Tommy Sadiq Hinrichsen on 06/03/14.
+//  Copyright (c) 2014 Tommy Sadiq Hinrichsen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Kal_Kya_PakayeTests : XCTestCase
+
+@end
+
+@implementation Kal_Kya_PakayeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
